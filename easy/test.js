82fa35a4d97f2c2c943e7b4a639faf5a6026cf27
oovaa <omarvx211@gmail.com>
2024-06-30 15:27:16 +0200

    add Valid Anagram  at 30-15

--- easy/test.js
+++ easy/test.js
@@ -1,6 +1,12 @@
-let o = {
-  1: 0,
-  2: 1
+function areSetsEqual(set1, set2) {
+  if (set1.size !== set2.size) return false
+  for (let item of set1) {
+    if (!set2.has(item)) return false
+  }
+  return true
 }
 
-console.log(o[3]);
+// Example usage
+const setA = new Set([1, 2, 3])
+const setB = new Set([3, 2, 1])
+console.log(areSetsEqual(setA, setB)) // Output: true
